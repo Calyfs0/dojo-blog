9ce6a86503f552f0cea5825e4273f755aa0a4619
Sagar Gurung <sagarthemaster@gmail.com>
2022-05-15 20:42:12 +0530

    Learning react day 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar";
+import MainContent from "./components/maincontent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-bar">
+        <Navbar />
+        <div className="main-content">
+          <MainContent />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/maincontent.js
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <div className="main-content">
+      <h1>This is main content</h1>
+    </div>
+  );
+}
+
+export default MainContent;
