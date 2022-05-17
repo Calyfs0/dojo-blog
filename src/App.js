63cce2f0aa4b0d1b7cd1814708b9b43251b47ad4
Sagar Gurung <sagarthemaster@gmail.com>
2022-05-17 22:21:36 +0530

    Using hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import "./App.css";
 import Navbar from "./components/navbar";
 import MainContent from "./components/maincontent";
 
 function App() {
   return (
     <div className="App">
-      <div className="nav-bar">
-        <Navbar />
-        <div className="main-content">
-          <MainContent />
-        </div>
-      </div>
+      <Navbar />
+      <MainContent />
     </div>
   );
 }
--- src/components/maincontent.js
+++ src/components/maincontent.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
 function MainContent() {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Introduction",
+      author: "Sagar Gurung",
+      content: "My journey on this earth...",
+      id: 1,
+    },
+    {
+      title: "My first day at school",
+      author: "Sagar Gurung",
+      content:
+        "I never wanted to go to school just like any other child but...",
+      id: 2,
+    },
+    {
+      title: "Life without parents",
+      author: "Sagar Gurung",
+      content: "I left my parents at the age of 7...",
+      id: 3,
+    },
+
+    {
+      title: "Life in Dehradun",
+      author: "Sagar Gurung",
+      content: "Staying in Dehradun without my parents wasn't easy...",
+      id: 3,
+    },
+  ]);
   return (
-    <div className="main-content">
-      <h1>This is main content</h1>
+    <div className="content">
+      {blogs.map((blog) => (
+        <div className="blog-preview" id={blog.id}>
+          <h2>{blog.title}</h2>
+          <h5>written by {blog.author}</h5>
+          <br />
+          <p>
+            {blog.content} <a href="/">Read More</a>
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
